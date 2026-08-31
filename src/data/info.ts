31b2c7993d025386c2341ec6d30e16e84c60575f
Riley <rmcaldwell2015@gmail.com>
2026-08-31 14:14:24 -0400

    Adding contactInfo and legal stuff

---
+++ src/data/info.ts
@@ -0,0 +1,7 @@
+export const contactInfo = {
+  businessNameFormal: "Dockside Web Solutions, LLC",
+  businessName: "Dockside Web Solutions",
+  phone: "(443) 844-7850",
+  phoneHref: "tel:+14438447850",
+  email: "contact@docksideweb.com"
+};
